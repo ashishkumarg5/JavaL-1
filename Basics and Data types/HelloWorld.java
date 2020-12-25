44fe6f314309ebdf5c2cc03f80bc969492d93cb8
Ashish <ashishkumarg5>
2020-12-25 18:33:26 +0530

    Java Basics

---
+++ Basics and Data types/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld{
+	//main method always take one parameter - String array
+	public static void main(String args[]){
+		System.out.println("Hello World!");
+	}	
+}
---
+++ Basics and Data types/HelloWorld1.java
@@ -0,0 +1,7 @@
+public class HelloWorld1{
+	//main method always take one parameter - String array
+	//x is string array which contains runtine parameters
+	public static void main(String x[]){
+		System.out.println("Hello " + x[5]);
+	}	
+}
